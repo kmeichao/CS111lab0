edecebd5382968486ce3f3ea2376e32d8c3e7c5d
kmeichao <kayleemeichao@gmail.com>
2023-01-16 22:04:02 -0800

    proc_count editted

--- lab0/proc_count.c
+++ lab0/proc_count.c
@@ -4,14 +4,28 @@
 #include <linux/seq_file.h>
 #include <linux/sched.h>
 
+struct proc_dir_entry *proc_entry = NULL;
+
+static int proc_count(struct seq_file *m, void *v)
+{
+	struct task_struct *t;
+	int count = 0;
+	for_each_process(t) {
+		count++;
+	}
+	seq_printf(m, "%d\n", count);
+}
+
 static int __init proc_count_init(void)
 {
 	pr_info("proc_count: init\n");
+	proc_entry = proc_create_single("count", 0, NULL, proc_count);
 	return 0;
 }
 
 static void __exit proc_count_exit(void)
 {
+	proc_remove(proc_entry);
 	pr_info("proc_count: exit\n");
 }
 
